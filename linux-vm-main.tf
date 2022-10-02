470a1d07c9521e6cf2734cf1db2900dec1b3c670
unknown <eslamshehata476@gmail.com>
2022-10-02 09:27:01 +0200

    deploy - terraform scripts to deploy vm and resources in azure

---
+++ linux-vm-main.tf
@@ -0,0 +1,6 @@
+source_image_reference {
+  publisher = var.ubuntu-publisher
+  offer     = var.ubuntu-offer
+  sku       = "18.04-LTS" 
+  version   = "latest"
+}
---
+++ network-main.tf
@@ -0,0 +1,19 @@
+# Create a resource group for network
+resource "azurerm_resource_group" "network-rg" {
+  name = "network-rg"
+  location = var.location
+}
+# Create the network VNET
+resource "azurerm_virtual_network" "network-vnet" {
+  name = "network-vnet"
+  address_space = [var.network-vnet-cidr]
+  resource_group_name = azurerm_resource_group.network-rg.name
+  location = azurerm_resource_group.network-rg.location
+}
+# Create a subnet for VM
+resource "azurerm_subnet" "vm-subnet" {
+  name = "vm-subnet"
+  address_prefixes = [var.vm-subnet-cidr]
+  virtual_network_name = azurerm_virtual_network.network-vnet.name
+  resource_group_name  = azurerm_resource_group.network-rg.name
+}
